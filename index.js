ca23cdf8bbe931b1d1ecf5abd2dac0a7a3a44ff4
manish singh <manish.singh.engg@gmail.com>
2023-08-26 15:42:25 +0530

    index js file commited

---
+++ index.js
@@ -0,0 +1,27 @@
+let sum=(a,b)=>{
+    return a+b;
+}
+
+let add=function(a,b){
+console.log(a+b);
+}
+
+let student={
+sub:function(a,b)
+{
+    console.log(a-b);
+}
+};
+
+let employee={
+    rollcode:25,
+    Getdata()
+    {
+        console.log(`student roll is ${this.rollcode}`);
+    }
+    };
+employee.Getdata();
+student.sub(50,49);
+add(20,30);
+
+//console.log(sum(10,30));
